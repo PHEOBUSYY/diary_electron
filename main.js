22c0b9c99ed662e17edac0743a4f1f5cf5462ff1
yanyi <yanyi609@126.com>
2018-07-11 17:33:20 +0800

    mongodb support

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
 const electron = require('electron');
 // const Menu = require("menu");
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+
+
+//mongodb
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', (error, db) => {
+    if (error)
+        console.log(error);
+    db.on('error',err=>{
+        console.log("db", err);
+    })
+});
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 1024, height: 678});
 
+    mainWindow.webContents.openDevTools();
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'www/index.html'),
         protocol: 'file:',
         slashes: true
-    }))
+    }));
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -38,16 +51,16 @@ function createWindow() {
         // Create our menu entries so that we can use MAC shortcuts
         electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate([{
             label: "Application",
-                submenu: [
-                    {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
-                    {type: "separator"},
-                    {
-                        label: "Quit", accelerator: "Command+Q", click: function () {
-                            app.quit();
-                        }
+            submenu: [
+                {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
+                {type: "separator"},
+                {
+                    label: "Quit", accelerator: "Command+Q", click: function () {
+                        app.quit();
                     }
-                ]
-             },
+                }
+            ]
+        },
             {
                 label: 'Edit',
                 submenu: [
@@ -64,12 +77,40 @@ function createWindow() {
             }
         ]));
     }
+
+    //测试mongodb是否连接成功
+    let dbhelper = new TargetDbHelper();
+    let time = '2018-7-9';
+    let targets = [];
+    targets.push({
+        text: '测试',
+        star: true,
+        editable: false,
+        type: 0,
+        week: ['周六','周日']
+    });
+    targets.push({
+        text: '测试4',
+        star: false,
+        editable: false,
+        type: 0,
+        week: ['周一','周五']
+    });
+    targets.push({
+        text: '测试5',
+        star: false,
+        editable: false,
+        type: 0,
+        week: ['周','周五']
+    });
+    dbhelper.createOrUpdate(time, targets, (res) => {
+    });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -78,7 +119,7 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
@@ -86,4 +127,77 @@ app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
+
+electron.ipcMain.on('target', (event, method, time, targets) => {
+    console.log(arguments);
+    let channel = 'targetRenderer';
+    if (method && time) {
+        let dbhelper = new TargetDbHelper();
+        if (method === 'get') {
+            dbhelper.get(time, (res) => {
+                event.sender.send(channel, method, time,res ? res.toJSON() : res);
+            });
+        } else if (method === 'create') {
+            dbhelper.createOrUpdate(time, targets, (res) => {
+                event.sender.send(channel, method, time, res ? res.toJSON() : res);
+            });
+        } else if (method === 'delete') {
+            dbhelper.del(time, (res) => {
+                event.sender.send(channel, method, time,res ?  res.toJSON() : res);
+            });
+        }
+    }
+
+});
+let Schema = mongoose.Schema;
+//构造函数
+let targetSchema = new Schema({
+    time: String,
+    targets: [{
+        text: String,
+        star: Boolean,
+        editable: Boolean,
+        type: {type: Number, min: 0, max: 5},
+        week: [String]
+    }]
+});
+
+let targetModel = mongoose.model('TargetModel', targetSchema);
+
+function TargetDbHelper() {
+    return {
+        get: function (time, callback) {
+            targetModel.findOne({
+                'time': time
+            }, function (err, res) {
+                if (err) console.log("get err", err);
+                console.log("res", res);
+                callback(res);
+            })
+        },
+        createOrUpdate: function (time, targets, callback) {
+            this.del(time,(err)=>{
+                if(err) console.log("del", err);
+                let newObject = new targetModel({
+                    time: time,
+                    targets: targets
+                });
+                console.log("save targets", JSON.stringify(targets));
+                newObject.save().then(res =>{
+                    console.log("save res", res);
+                    callback(res);
+                }).catch(err =>{
+                    if(err) console.log("save", err);
+                })
+            })
+        },
+        del: function (time, callback) {
+            targetModel.deleteOne({'time': time}, function (err, res) {
+                if (err) console.log("del err", err);
+                console.log("res", res);
+                callback(res);
+            })
+        }
+    }
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+//在渲染器进程 (网页) 中。
+
+const electron = require('electron');
+global.electron = electron;
+
+
