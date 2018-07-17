ce19ae1566c9c7ddc27a467e2866e3f03a06fd0e
yanyi <yanyi609@126.com>
2018-07-17 11:10:03 +0800

    refactor template

---
+++ dbhelper.js
@@ -0,0 +1,155 @@
+/**
+ * 所有的数据库操作都放在这里
+ * @type {module:mongoose}
+ */
+//mongodb
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', {
+    // useNewUrlParser: true
+}, (error, db) => {
+    if (error) console.log(error);
+    db.on('error', err => {
+        console.log("db", err);
+    })
+});
+
+let Schema = mongoose.Schema;
+//目标对象
+let targetSchema = new Schema({
+    time: String,
+    targets: [{
+        text: String,
+        star: Boolean,
+        editable: Boolean,
+        type: {type: Number, min: 0, max: 5},
+        week: [String]
+    }]
+});
+
+function TargetDbHelper(method, time, targets, callback) {
+    let targetModel = mongoose.model('TargetModel', targetSchema);
+
+
+    function get(time, callback) {
+        targetModel.findOne({
+            'time': time
+        }, function (err, res) {
+            if (err) console.log("get err", err);
+            callback(res);
+        })
+    }
+
+    function createOrUpdate(time, targets, callback) {
+        del(time, (err) => {
+            if (err) console.log("del", err);
+            let newObject = new targetModel({
+                time: time,
+                targets: targets
+            });
+            newObject.save().then(res => {
+                callback(res);
+            }).catch(err => {
+                if (err) console.log("save", err);
+            })
+        })
+    }
+
+    function del(time, callback) {
+        targetModel.deleteOne({'time': time}, function (err, res) {
+            if (err) console.log("del err", err);
+            callback(res);
+        })
+    }
+
+    if (method && time) {
+        if (method === 'get') {
+            get(time, callback);
+        } else if (method === 'create') {
+            createOrUpdate(time, targets, callback);
+        } else if (method === 'delete') {
+            del(time, callback);
+        }
+    }
+}
+
+//成就，图片，感悟
+let inputGroupSchema = new Schema({
+    time: Date,//日期
+    type: {type: Number, min: 0, max: 10},//类型  1: 成就 2：感悟 3： 照片 4: 标题 5: ...
+    data: [{
+        value: String,//输入值
+    }]
+});
+
+//成就，图片，感悟
+function InputGroupHelper(args, callback) {
+    let inputGroupModel = mongoose.model('InputGroupModel', inputGroupSchema);
+
+    function get(time, type, callback) {
+        inputGroupModel.findOne({
+            'time': new Date(time),//日期
+            'type': type,//类型
+        }, function (err, res) {
+            if (err) console.log("get err", err);
+            callback(res);
+        })
+    }
+
+    function createOrUpdate(time, type, data, callback) {
+        del(new Date(time), type, (err) => {
+            if (err) console.log("del", err);
+            let newObject = new inputGroupModel({
+                time: new Date(time),
+                type: type,
+                data: data
+            });
+            newObject.save().then(res => {
+                callback(res);
+            }).catch(err => {
+                if (err) console.log("save", err);
+            })
+        })
+    }
+
+    function del(time, type, callback) {
+        inputGroupModel.deleteOne({'time': new Date(time), 'type': type}, function (err, res) {
+            if (err) console.log("del err", err);
+            callback(res);
+        })
+    }
+
+    let method = args.method;
+    let time = args.time;
+    let type = args.type;
+    if (method === 'get') {
+        get(time, type, callback);
+    } else if (method === 'create') {
+        let data = args.data;
+        createOrUpdate(time, type, data, callback);
+    } else if (method === 'delete') {
+        del(time, type, callback);
+    }
+}
+
+
+const dbHelper = {
+    dbTarget: function (event, method, time, targets) {
+        let targetRenderKey = 'targetRenderer';//renderer线程接受的key
+        let callback = res => {
+            event.sender.send(targetRenderKey, method, time, res ? res.toJSON() : res);
+        };
+        TargetDbHelper(method, time, targets, callback);
+    },
+    dbInputGroup: function (event, args) {
+        let inputGroupRenderKey = 'inputGroupRenderer';//renderer线程接受的key
+        let callback = res => {
+            event.sender.send(inputGroupRenderKey, args, res ? res.toJSON() : res);
+        };
+        InputGroupHelper(args, callback);
+    },
+    test: function (args, callback) {
+        InputGroupHelper(args, callback)
+    }
+
+};
+module.exports = dbHelper;
--- main.js
+++ main.js
@@ -13,20 +13,12 @@ const url = require('url');
 let mainWindow;
 
 
-//mongodb
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database', (error, db) => {
-    if (error)
-        console.log(error);
-    db.on('error',err=>{
-        console.log("db", err);
-    })
-});
+//dbHelper
+let dbHelper = require('./dbhelper');
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 1024, height: 678});
+    mainWindow = new BrowserWindow({width: 1500, height: 750});
 
     mainWindow.webContents.openDevTools();
     // and load the index.html of the app.
@@ -79,32 +71,23 @@ function createWindow() {
     }
 
     //测试mongodb是否连接成功
-    let dbhelper = new TargetDbHelper();
-    let time = '2018-7-9';
-    let targets = [];
-    targets.push({
-        text: '测试',
-        star: true,
-        editable: false,
-        type: 0,
-        week: ['周六','周日']
-    });
-    targets.push({
-        text: '测试4',
-        star: false,
-        editable: false,
-        type: 0,
-        week: ['周一','周五']
-    });
-    targets.push({
-        text: '测试5',
-        star: false,
-        editable: false,
-        type: 0,
-        week: ['周','周五']
-    });
-    dbhelper.createOrUpdate(time, targets, (res) => {
-    });
+    let args = {
+      method: 'get',
+      time : new Date().toLocaleDateString(),
+      type: 1,
+      data: [
+          {
+              value: '测试0'
+          },
+          {
+              value: '测试1'
+
+          }
+      ]
+    };
+    dbHelper.test(args, res =>{
+        console.log("tester", res);
+    })
 }
 
 // This method will be called when Electron has finished
@@ -128,76 +111,15 @@ app.on('activate', function () {
         createWindow()
     }
 });
-
-electron.ipcMain.on('target', (event, method, time, targets) => {
-    console.log(arguments);
-    let channel = 'targetRenderer';
-    if (method && time) {
-        let dbhelper = new TargetDbHelper();
-        if (method === 'get') {
-            dbhelper.get(time, (res) => {
-                event.sender.send(channel, method, time,res ? res.toJSON() : res);
-            });
-        } else if (method === 'create') {
-            dbhelper.createOrUpdate(time, targets, (res) => {
-                event.sender.send(channel, method, time, res ? res.toJSON() : res);
-            });
-        } else if (method === 'delete') {
-            dbhelper.del(time, (res) => {
-                event.sender.send(channel, method, time,res ?  res.toJSON() : res);
-            });
-        }
-    }
-
+//目标
+let targetKey = 'target';
+electron.ipcMain.on(targetKey, (event, method, time, targets) => {
+    dbHelper.dbTarget(event, method, time, targets);
 });
-let Schema = mongoose.Schema;
-//构造函数
-let targetSchema = new Schema({
-    time: String,
-    targets: [{
-        text: String,
-        star: Boolean,
-        editable: Boolean,
-        type: {type: Number, min: 0, max: 5},
-        week: [String]
-    }]
+//成就
+let inputGroupKey = 'inputGroup';
+electron.ipcMain.on(inputGroupKey, (event, args) => {
+    dbHelper.dbInputGroup(event, args);
 });
 
-let targetModel = mongoose.model('TargetModel', targetSchema);
 
-function TargetDbHelper() {
-    return {
-        get: function (time, callback) {
-            targetModel.findOne({
-                'time': time
-            }, function (err, res) {
-                if (err) console.log("get err", err);
-                console.log("res", res);
-                callback(res);
-            })
-        },
-        createOrUpdate: function (time, targets, callback) {
-            this.del(time,(err)=>{
-                if(err) console.log("del", err);
-                let newObject = new targetModel({
-                    time: time,
-                    targets: targets
-                });
-                console.log("save targets", JSON.stringify(targets));
-                newObject.save().then(res =>{
-                    console.log("save res", res);
-                    callback(res);
-                }).catch(err =>{
-                    if(err) console.log("save", err);
-                })
-            })
-        },
-        del: function (time, callback) {
-            targetModel.deleteOne({'time': time}, function (err, res) {
-                if (err) console.log("del err", err);
-                console.log("res", res);
-                callback(res);
-            })
-        }
-    }
-}
